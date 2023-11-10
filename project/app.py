52106fe73c99ea1b8a107539c95b2c935abc42d9
chat0933 <90390114+chat0933@users.noreply.github.com>
2023-11-10 11:39:57 +0100

    v.05
    Fist commit
    Created python files and some sudo code

---
+++ project/app.py
@@ -0,0 +1,1 @@
+# Brug bottle eller flask???
---
+++ sudo_code_v1.py
@@ -0,0 +1,40 @@
+"""
+HUSK!!!!!:
+AT LAVE ET VIRTUAL ENVIROMENT!!!
+NAVNE PÅ VARIABLER ER SKREVET MED SMÅT OG MELLEMRUMS ERSTATNING ER : _ dvs. her_er_et_variable_navn
+BRUG IKKE ENKELTE BOGSTAVER TIL VARIABLE NAVNE
+KALD FUNKTIONS NAVNENE NOGET SOM GIVER MENING I FORHOLD TIL HVAD DE GØR
+
+
+Hjertemåler
+checker pulsen på patinet
+if statements 
+hvis pulsen er for høj eller for lav så send en advarsel
+hvis den er tilpads så vær inaktiv
+
+
+Gyroskop:
+Hold øje med om personen står op eller er faldet
+if statements checker
+hvis de står op så gør ikke noget
+hvis de er faldet så skriv det ind i databasen med deres koodrinater
+
+Buzzer:
+anvender gyroskop data til at allarmere folk at der er en som er faldet
+if statement checker
+Hvis de står op så gør ikke noget
+hvis de er faldet så giv lyd
+(Vi skal lige blive enige om buzzeren skal give lyd i andre situationer)
+
+DB:
+modtager data fra vores komponenter og alt afhængig af hvad der er problemet giver den signal til lægerne
+hvis der kun er 1 problem så er resten af felterne tomme
+hvis der er 2 eller flere problemer så kommer de også frem på samme tid
+
+andet:
+Brug sleep så vi ikke spammer DB
+
+Hjemmside:
+Vis hvad der står i DB på hjemmesiden
+
+"""
