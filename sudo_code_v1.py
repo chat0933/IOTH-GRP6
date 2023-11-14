e023206296e18d0d9fe10cae294a01dbcf1b8324
chat0933 <90390114+chat0933@users.noreply.github.com>
2023-11-14 12:22:06 +0100

    Update sudo_code_v1.py
    Added more rules

--- sudo_code_v1.py
+++ sudo_code_v1.py
@@ -9,38 +9,48 @@ LAV KUN KLASSER HVIS DEN INDEHOLDER FLERE FUNKTIONER SOM SKAL BRUGES FORSKELLIGE
 
 
 Hjertemåler
-checker pulsen på patinet
-if statements 
-hvis pulsen er for høj eller for lav så send en advarsel
-hvis den er tilpads så vær inaktiv
-
+- Checker pulsen på patinet
+- if statements, med else if og else
+- Hvis pulsen er for lav (else if) så send en advarsel til database som viser advarslen på hjemmesiden
+og aktiver buzzer
+- Hvis den er tilpads så vær inaktiv (else)
+- MÅSKE: (for høj Så send til database)
 
 Gyroskop:
-Hold øje med om personen står op eller er faldet
-if statements checker
-hvis de står op så gør ikke noget
-hvis de er faldet så skriv det ind i databasen med deres koodrinater
+- Hold øje med om personen står op eller er faldet
+- if statements checker
+- Hvis de står op så gør ikke noget
+- Hvis de er faldet så skriv det ind i databasen med deres koodrinater fra GPS
+- Vis data på hjemmesiden
+- Se på om vi kan zigge Google Maps api :D
 
 Buzzer:
-anvender gyroskop data til at allarmere folk at der er en som er faldet
-if statement checker
-Hvis de står op så gør ikke noget
-hvis de er faldet så giv lyd
-(Vi skal lige blive enige om buzzeren skal give lyd i andre situationer)
+- Anvender Gyroskop data til at allarmere folk at der er en som er faldet
+- if statement checker
+- Hvis de står op så gør ikke noget
+- Hvis de er faldet så giv lyd
+- (Vi skal lige blive enige om buzzeren skal give lyd i andre situationer)
+- Hvis hjerterytme er lav så giv lyd!
 
 GPS:
-Fortæl lægerne hvor patienten befinde sig
-hvis der er hjerteproblemer eller personen er faldet så send data ind i DB
+- Fortæl lægerne hvor patienten befinde sig
+- Hvis der er hjerteproblemer eller personen er faldet så send data fra gps og hjerterytme/ om de er faldet ind i DB
 
 DB:
-modtager data fra vores komponenter og alt afhængig af hvad der er problemet giver den signal til lægerne
-hvis der kun er 1 problem så er resten af felterne tomme
-hvis der er 2 eller flere problemer så kommer de også frem på samme tid
+- Modtager data fra vores komponenter og alt afhængig af hvad der er problemet opdatere den hjemmesiden og allarmere sunhedspersonalet
+- Vi laver en enkelt database hvori alt fra vores sensorer bliver sendt til i den samme table
+- Hvis fx hjerterytme er okay men patienten er faldet, så send alt data til database
+og vis på hjemmesiden at hjerterytme er stabil men patienten er faldet samt deres GPS koordinat :D
+- GPS skal altid sendes!!!! hvis der opstår noget
 
-andet:
-Brug sleep så vi ikke spammer DB
+Andet:
+- Brug sleep så vi ikke spammer DB
+- Husk at åben og luk DB for sikkerheds skyld og for at den kan opdateres
 
 Hjemmside:
-Vis hvad der står i DB på hjemmesiden
-
+- Vis hvad der står i DB på hjemmesiden
+- Aktuator: En form for alarm når der opstår et problem (RØD SKÆRM / LYD)
+- login (MFA,)
+- https: Vi vil opnå https gennem hosting på azure appservice
+- MQTT sender vores data fra database til hjemmeside
 """
