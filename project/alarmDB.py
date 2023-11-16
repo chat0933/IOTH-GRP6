29135d0c8ba373b807774ac4356366257eecc12c
chat0933 <90390114+chat0933@users.noreply.github.com>
2023-11-16 16:18:43 +0100

    PatientDB v1
    Made a test DB that insert specified values into a databse

---
+++ project/alarmDB.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+PATIENT_ID = 1
+FALLEN = "TRUE"
+HEARTH_RATE = 123
+GPS1 = 666.666
+GPS2 = 6969.6969
+
+print("HEY")
+
+database = ('patient_database.db')
+
+con = sqlite3.connect(database)
+cur = con.cursor()
+
+def create_patient_table():
+    cur.execute('CREATE TABLE IF NOT EXISTS Issues(PATIENT_ID INT, FALLEN TEXT, HEARTH_RATE INT, GPS1 REAL, GPS2 REAL)')
+    
+
+create_patient_table()
+
+def test_insert():
+    cur.execute('INSERT INTO Issues(PATIENT_ID, FALLEN, HEARTH_RATE, GPS1, GPS2) VALUES (?, ?, ?, ?, ?)', (PATIENT_ID,FALLEN, HEARTH_RATE,GPS1,GPS2))
+    con.commit()
+    cur.close()
+    print("Inserted test into db")
+
+test_insert()
