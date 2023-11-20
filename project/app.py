dd7a3ea7e69aff50a1bcaec09a0fc51a8090f5b9
Greeznerd <christopherhaugaard@gmail.com>
2023-11-20 14:10:09 +0100

    Ny hjemmeside
    Mangler at blive forbundet til database. Mangler at lave register page.

--- project/app.py
+++ project/app.py
@@ -1,1 +1,48 @@
-# Brug bottle eller flask???
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "abemadXD"
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/register", methods= ["POST", "GET"])
+
+
+
+@app.route("/login", methods= ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash(f"Velkommen til Rynkeby, Mr. {user}")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash(f"Du er allerede logget ind {user}! Er du gammel eller hva?")
+            return redirect(url_for("user"))
+        
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("Hej! Hvem er du?")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    flash("Tak for denne gang, vi savner dig allerede", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
