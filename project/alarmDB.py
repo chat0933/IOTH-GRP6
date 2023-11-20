c0a555f15ae87ee4b170bde7ed74f799888d06f1
chat0933 <90390114+chat0933@users.noreply.github.com>
2023-11-20 20:55:52 +0100

    Added new functions
    Added and tested a function that adds a new user

--- project/alarmDB.py
+++ project/alarmDB.py
@@ -1,14 +1,12 @@
 import sqlite3
+# Skriv ind i DB så snart der opstår et problem uanset om det er hjerte releateret eller om de er fladet
 
-
-
-"""
 PATIENT_ID = 1
 FALLEN = "TRUE"
 HEARTH_RATE = 123
 GPS1 = 666.666
 GPS2 = 6969.6969
-"""
+
 
 print("HEY")
 
@@ -19,18 +17,26 @@ cur = con.cursor()
 
 def create_patient_table():
     cur.execute('CREATE TABLE IF NOT EXISTS Issues(PATIENT_ID INT, FALLEN TEXT, HEARTH_RATE INT, GPS1 REAL, GPS2 REAL)')
+    con.commit
 
 def create_user_table():
-    cur.execute('CREATE TABLE IF NOT EXISTS Users(USERNAME TEXT, PASSWORD TEXT)')    
+    cur.execute('CREATE TABLE IF NOT EXISTS Users(uid INTEGER PRIMARY KEY,USERNAME TEXT, PASSWORD TEXT)')
+    con.commit    
 
-#create_patient_table()
-#create_user_table()
+def create_user_account(username,password): #Use this function to save the created user from app.py into our database
+    cur.execute('INSERT INTO users(username, password) VALUES (?, ?)', (username, password))
+    con.commit
+
+create_patient_table()
+create_user_table()
 
 def test_insert():
+    username = "hejsa1"
+    password = "test2"
     cur.execute('INSERT INTO Issues(PATIENT_ID, FALLEN, HEARTH_RATE, GPS1, GPS2) VALUES (?, ?, ?, ?, ?)', (PATIENT_ID,FALLEN, HEARTH_RATE,GPS1,GPS2))
+    create_user_account(username,password)
     con.commit()
     cur.close()
     print("Inserted test into db")
 
-#test_insert()
-
+test_insert()
